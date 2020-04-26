9ec3218cc3dd547d4fc7323ead002c9ba07f1e07
IamMoondance <daria-artiste@mail.ru>
2020-04-26 21:02:07 +0300

    Добавление автодокументации
    Автодокументация на основе библиотеки Sphinx.

--- db.py
+++ db.py
@@ -1,3 +1,18 @@
+"""
+@author: Шибанова Д.А., ИУ7-82Б
+
+Файл работы с базой данных.
+
+Техническое задание:
+Разработать десктоп-приложение для подбора снимков по тегам.
+Предусмотреть перечень тегов, которые могут быть установлены
+пользователем вручную. Пользователь может:
+загрузить снимки в систему;
+указать теги из числа предложенных вручную;
+установить автоопределение тегов на основе анализа снимка;
+производить поиск по тегу из перечня фильтров.
+"""
+
 import sqlite3
 import json
 
@@ -7,6 +22,17 @@ cursor = conn.cursor()
 
 # Создание таблицы
 def create_table(cursor):
+    """
+    Создаёт таблицу в базе данных sqlite3 с тремя столбцами.
+
+    :param cursor: курсор
+    :type pic_id: integer primary key
+    :type tags: json
+    :type pic_link: text
+    :rtype: None
+    
+    """
+    
     cursor.execute("""
     CREATE TABLE pictures
     (pic_id INTEGER PRIMARY KEY, tags json, pic_link TEXT)
@@ -14,6 +40,14 @@ def create_table(cursor):
 
 #Удаление таблицы
 def delete_table(cursor):
+    """
+    Удаляет таблицу со снимками.
+
+    :param cursor: курсор
+    :rtype: None
+    
+    """
+    
     cursor.execute("""
     DROP TABLE pictures
     """)
@@ -22,6 +56,21 @@ def delete_table(cursor):
 
 ## Добавление данных в БД
 def insert_data(conn, cursor, pic_id, pic_tags, pic_link):
+    """
+    Добавляет одну строку данных в БД.
+
+    :param conn: соединение
+    :param cursor: курсор
+    :param pic_id: идентификатор снимка
+    :param pic_tags: теги изображения
+    :param pic_link: адрес изображения
+    :type pic_id: integer primaty key (int)
+    :type pic_tags: json
+    :type pic_link: text (str)
+    :rtype: None
+    
+    """
+    
     cursor.execute("""
     INSERT INTO pictures
     VALUES (""" + pic_id + """, '[""" + pic_tags + """]', '""" + pic_link + \
@@ -30,6 +79,21 @@ def insert_data(conn, cursor, pic_id, pic_tags, pic_link):
 
 # Добавление множества данных в БД одновременно через метод "?"
 def many_insert_data(conn, cursor, id_list, tag_list, link_list):
+    """
+    Добавляет одновременно более одной строки данных в БД через метод "?"
+
+    :param conn: соединение
+    :param cursor: курсор
+    :param id_list: список идентификаторов
+    :param tag_list: список тегов
+    :param link_list: список ссылок на снимок
+    :type id_list: массив integer'ов
+    :type tag_list: массив json
+    :type link_list: массив строк
+    :rtype: None
+    
+    """
+    
     len_id = len(id_list)
     if len_id == len(tag_list) and len_id == len(link_list):
         for i in range(len_id-1):
@@ -44,6 +108,13 @@ def many_insert_data(conn, cursor, id_list, tag_list, link_list):
 
 # Вывод всего содержимого БД
 def select_all(cursor):
+    """
+    Выводит все строки БД.
+
+    :param cursor: курсор
+    :rtype: None
+    """
+    
     cursor.execute("SELECT * FROM pictures")
     rows = cursor.fetchall()
     for row in rows:
@@ -51,10 +122,21 @@ def select_all(cursor):
 
 # Вывод содержимого, которое соотвествует заданному параметру
 def select_by_param():
+    """
+    Выводит все строки, соответствующие заданному параметру.
+
+    
+    """
+    
     pass
 
 # Обновление БД
 def update_table():
+    """
+    Обновляет БД.
+    
+    """
+    
     pass
 ##upd_sql = """
 ##UPDATE pictures
@@ -65,8 +147,15 @@ def update_table():
 
 # Удаление из БД
 def delete_table():
+    """
+    Удаляет заданную строку из БД.
+    """
+    
     pass
 ##del_sql = "DELETE FROM pictures WHERE = ''"
 ##cursor.execute(del_sql)
 
 conn.close()
+
+if __name__ == "__main__":
+    pass
---
+++ source/index.rst
@@ -0,0 +1,23 @@
+.. DB_docs documentation master file, created by
+   sphinx-quickstart on Sun Apr 26 20:04:22 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DB_docs's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: db
+  :members:
+  :show-inheritance:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
