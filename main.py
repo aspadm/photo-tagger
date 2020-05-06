d9365d98fe5d0b57bac35600248b32d4351a0e9d
Alexey Kirillov <asp-adm@yandex.ru>
2020-05-06 10:48:05 +0300

    ui: прототип базового класса Минимальный прототип работающего приложения.
    Signed-off-by: Alexey Kirillov <asp-adm@yandex.ru>

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+
+""" TODO:
+mainwindow
+    splitter
+        scrollarea (horizontal layout)
+            checkbox (tag)
+        scrollarea (grid layout)
+            photowidget (2 labels)
+"""
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle("Photo tagger")
+        
+        self.show()
+        
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Exit',
+            "Are you sure to quit?",
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = MainWindow()
+
+    sys.exit(app.exec_())
